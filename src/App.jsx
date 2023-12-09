2f408a38a2aa4165f96a0b8acd0408e4c7c43f5b
iostream-code <aryakmifjannah2603@gmail.com>
2023-12-10 03:01:24 +0700

    adding some file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import Dashboard from "./pages/Dashboard";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/dasboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/dasboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
   );
 }
 
