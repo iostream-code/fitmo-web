74addea61923365cc1c06dbf01bfa73fa5b0b883
iostream-code <aryakmifjannah2603@gmail.com>
2023-12-10 08:45:35 +0700

    adinng some view

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import Dashboard from "./pages/Dashboard";
 import Welcome from "./pages/Welcome";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -10,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Welcome />} />
           <Route path="/dasboard" element={<Dashboard />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
     </ChakraProvider>
